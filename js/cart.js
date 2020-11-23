3805e2cce1ad6e342299e5587223e48b9017345c
minhquandhtb <minhquan211297@gmail.com>
2020-11-23 23:04:58 +0700

    third commit

--- js/cart.js
+++ js/cart.js
@@ -57,6 +57,60 @@ function clearCart() {
   localStorage.removeItem("cart");
   location.reload();
 }
+function pay(){
+  var h1=document.createElement("h1");
+  h1.innerHTML="Thanh toán";
+  document.getElementById("pay").appendChild(h1);
+  var form=document.createElement("form");
+
+  var cod=document.createElement("div");
+  var radioCod=document.createElement("input");
+  radioCod.setAttribute("type","radio");
+  radioCod.setAttribute("id","cod");
+  radioCod.setAttribute("name","pay")
+  radioCod.setAttribute("value","cod");
+  var labelCod=document.createElement("label");
+  labelCod.setAttribute("for","cod");
+  labelCod.innerHTML="Thanh toán tiền mặt";
+  cod.appendChild(radioCod);
+  cod.appendChild(labelCod);
+
+  var bank=document.createElement("div");
+  var radioBank=document.createElement("input");
+  radioBank.setAttribute("type","radio");
+  radioBank.setAttribute("id","bank");
+  radioBank.setAttribute("name","pay")
+  radioBank.setAttribute("value","bank");
+  var labelBank=document.createElement("label");
+  labelBank.setAttribute("for","bank");
+  labelBank.innerHTML="Thanh toán qua ngân hàng";
+  bank.appendChild(radioBank);
+  bank.appendChild(labelBank);
+
+  var eWallet=document.createElement("div");
+  var radioEWallet=document.createElement("input");
+  radioEWallet.setAttribute("type","radio");
+  radioEWallet.setAttribute("id","e-wallet");
+  radioEWallet.setAttribute("name","pay")
+  radioEWallet.setAttribute("value","e-wallet");
+  var labelEWallet=document.createElement("label");
+  labelEWallet.setAttribute("for","e-wallet");
+  labelEWallet.innerHTML="Thanh toán qua ví điện tử";
+  eWallet.appendChild(radioEWallet);
+  eWallet.appendChild(labelEWallet);
+  
+  
+  form.appendChild(cod);
+  form.appendChild(bank);
+  form.appendChild(eWallet);
+
+  document.getElementById("pay").appendChild(form);
+  document.querySelector("#step-2 .circle").style.borderColor=("orange");
+  document.querySelector("#step-2 .rectangle").style.backgroundColor=("orange");
+  document.getElementById("address").style.display=("none");
+  document.getElementById("btn-buy").innerHTML="Đặt hàng";
+  document.getElementById("btn-buy").setAttribute("onclick", "buy()");
+}
 function address(){
   var h1=document.createElement("h1");
   h1.innerHTML="Nhập địa chỉ";
@@ -79,8 +133,11 @@ function address(){
   form.appendChild(phoneNumber);
   form.appendChild(address);
   document.getElementById("address").appendChild(form);
-  document.getElementById("btn-buy").innerHTML="Đặt hàng";
-  document.getElementById("btn-buy").setAttribute("onclick", "buy()");
+  document.getElementById("list-cart").style.display=("none");
+  document.getElementById("process").style.display=("flex");
+  document.querySelector("#step-1 .circle").style.borderColor=("orange");
+  document.getElementById("btn-buy").innerHTML="Thanh toán";
+  document.getElementById("btn-buy").setAttribute("onclick", "pay()");
 
 }
 
